4405b94bf358edfe9c40f88c909cc78ad0c1dfa4
syf <2799263602@qq.com>
2018-06-12 19:23:00 +0800

    section

--- team2_MT/src/js/main.js
+++ team2_MT/src/js/main.js
@@ -1,12 +1,12 @@
 require.config({
-	baseUrl:"/js/",
-	paths:{
-		//库
-		"jquery":"lib/jquery",
-		"bscroll":"lib/bscroll.min",
-		"swiper":"lib/swiper.min",
-		//mysealf
-		"index":"app/index"
-	}
+    baseUrl: "/js/",
+    paths: {
+        //库
+        "jquery": "lib/jquery",
+        "bscroll": "lib/bscroll.min",
+        "swiper": "lib/swiper.min",
+        //mysealf
+        "index": "app/index"
+    }
 })
-document.documentElement.style.fontSize=window.innerWidth/750*100+"px";
+document.documentElement.style.fontSize = window.innerWidth / 750 * 100 + "px";
